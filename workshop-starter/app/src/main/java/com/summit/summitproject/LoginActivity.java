c2a06712d44c8a5a226c0222b9e0e23af77062a1
Pranav Mahesh <pmahesh@gatech.edu>
2020-05-18 15:52:25 -0400

    Add UI behavior

--- workshop-starter/app/src/main/java/com/summit/summitproject/LoginActivity.java
+++ workshop-starter/app/src/main/java/com/summit/summitproject/LoginActivity.java
@@ -1,6 +1,14 @@
 package com.summit.summitproject;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.summit.summitproject.prebuilt.login.LoginManager;
@@ -12,14 +20,58 @@ import com.summit.summitproject.prebuilt.login.LoginManager;
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText username;
+    private EditText password;
+    private Button signIn;
+    private ProgressBar progressBar;
+
     /**
      * Called the first time an Activity is created, but before any UI is shown to the user.
      * Prepares the layout and assigns UI widget variables.
+     * Initialization
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize fields and connect them to XML
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signIn = findViewById(R.id.signIn);
+        progressBar = findViewById(R.id.progressBar);
+
+        // Disable button
+        signIn.setEnabled(false);
+
+        username.addTextChangedListener(textWatcher);
+        password.addTextChangedListener(textWatcher);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Login Clicked!", Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        });
     }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String inputedUsername = username.getText().toString().trim();
+            String inputedPassword = password.getText().toString().trim();
+            boolean enabled = !inputedUsername.isEmpty() && !inputedPassword.isEmpty();
+            signIn.setEnabled(enabled);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+    };
 
 }
